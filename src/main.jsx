dbac55edffb7754f73031514697d6b64859235c3
reichenbakmh-hash <reichenbakmh@gmail.com>
2026-08-15 09:38:02 +0300

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import QGApp from "./app/QGApp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <QGApp />
-  </React.StrictMode>
-);
